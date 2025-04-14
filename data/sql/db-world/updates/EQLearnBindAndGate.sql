d9e5428f371a6182c4f35748a4a254c784ef77d9
NathanHandley <nathanhandley@protonmail.com>
2025-04-14 15:20:01 -0500

    Learn bind and gate by default at level 1

---
+++ data/sql/db-world/updates/EQLearnBindAndGate.sql
@@ -0,0 +1,2 @@
+INSERT INTO `playercreateinfo_spell_custom` (`raceMask`, `classMask`, `spell`, `note`) VALUES (0, 0, 86900, 'Gate');
+INSERT INTO `playercreateinfo_spell_custom` (`raceMask`, `classMask`, `spell`, `note`) VALUES (0, 0, 86901, 'Bind Self');
