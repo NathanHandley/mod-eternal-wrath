6d5537433055f7ef0fa52870623d03ef0563c04b
NathanHandley <nathanhandley@protonmail.com>
2025-04-16 06:49:37 -0500

    Add delete statement to bind/gate

--- data/sql/db-world/updates/EQLearnBindAndGate.sql
+++ data/sql/db-world/updates/EQLearnBindAndGate.sql
@@ -1,2 +1,3 @@
+DELETE FROM playercreateinfo_spell_custom WHERE spell IN (86900, 86901);
 INSERT INTO `playercreateinfo_spell_custom` (`raceMask`, `classMask`, `spell`, `note`) VALUES (0, 0, 86900, 'Gate');
 INSERT INTO `playercreateinfo_spell_custom` (`raceMask`, `classMask`, `spell`, `note`) VALUES (0, 0, 86901, 'Bind Self');
